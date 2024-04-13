d4f60e208771631cad922d6c20c15fa5d498f818
basmabadr1 <98922591+basmabadr1@users.noreply.github.com>
2024-04-13 19:25:57 +0200

    Add files via upload

---
+++ task/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+import 'SignUp.dart';
+import 'vir.dart';
+import 'forget.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Login App',
+    home: LoginPage(),
+  ));
+}
